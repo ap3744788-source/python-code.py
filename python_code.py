b883a9b709e8461a5828dbffef77da8970a138bc
Kartikey Pandey <ap3744788@gmail.com>
2026-06-28 12:53:55 +0530

    Add files via upload

---
+++ python_code.py
@@ -0,0 +1,23 @@
+print("1 - add")
+print("2 - subtract")
+print("3 - multiply")
+print("4 - divide")
+option = int(input("choose an operation:"))
+
+if(option in [1,2,3,4]):
+    num1=int(input("enter first number:"))
+    num2=int(input("enter second number:"))
+
+    if(option==1):
+        result=num1+num2
+    elif(option==2):
+        result=num1-num2
+    elif(option==3):
+        result=num1*num2 
+    elif(option==4):
+        result=num1//num2           
+
+else: 
+    print("invalid operation entered")
+
+print("the result of the operation is {}".format(result))    
